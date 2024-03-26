6d9f7ffe8e0343e60a16a81dfdb5a37cf360aa3e
TomoChen <stu71210@gmail.com>
2024-03-27 03:17:43 +0800

    Table Practice
    記得options要加等於阿......

--- main.lua
+++ main.lua
@@ -3,12 +3,31 @@
 -- Version: 2.0
 -- Sample code is MIT licensed; see https://solar2d.com/LICENSE.txt
 ---------------------------------------------------------------------------------------
-
+--[[
 local world = display.newImageRect( "world.png", 100, 100 )
 world.x = display.contentCenterX
 world.y = display.contentCenterY - 30
 
+local world2 = display.newimagerect("world.png",99,99)
+world2.x = display.contentCenterX
+world2.y = display.contentHeight * 0.85
+
 local msgText = display.newText( "Hello, world!", world.x, world.y+160, native.systemFont, 32 )
 msgText:setFillColor( 0.2, 0.6, 0.8 )
-local msgText2 = display.newText( "Hello, world!", world.x, world.y - 10, native.systemFont, 32 )
+local msgText2 = display.newText( "Hello, world!", world2.x, world2.y + 160, native.systemFont, 24 )
 msgText2:setFillColor( 0.2, 0.6, 0.8 )
+]]--
+
+
+nameTest = "TomoChen";
+local option =
+{
+	text = nameTest,
+	x = display.contentCenterX,
+	y = display.contentHeight * 0.5,
+	font = native.systemFontBold,
+	fontSize = 30, --之後改大小要從Size改，
+	align = "center" -- 記得雙引號
+}
+local tomochen = display.newText(option)
+tomochen:setFillColor( 0.2, 0.6, 0.8 )
