563cf9a9531be91ad57a66076b9121b3dc32aa90
TomoChen <stu71210@gmail.com>
2024-03-27 02:57:20 +0800

    新增Text物件
    多用了一個Text穿過地球，因為我爽呵呵

--- main.lua
+++ main.lua
@@ -4,9 +4,11 @@
 -- Sample code is MIT licensed; see https://solar2d.com/LICENSE.txt
 ---------------------------------------------------------------------------------------
 
-local world = display.newImageRect( "world.png", 250, 250 )
+local world = display.newImageRect( "world.png", 100, 100 )
 world.x = display.contentCenterX
 world.y = display.contentCenterY - 30
 
 local msgText = display.newText( "Hello, world!", world.x, world.y+160, native.systemFont, 32 )
 msgText:setFillColor( 0.2, 0.6, 0.8 )
+local msgText2 = display.newText( "Hello, world!", world.x, world.y - 10, native.systemFont, 32 )
+msgText2:setFillColor( 0.2, 0.6, 0.8 )
